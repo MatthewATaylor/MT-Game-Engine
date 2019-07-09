74decaae902700ce07e38a1984f49f1e19e336ee
Matthew Taylor <MatthewAlanTaylor@outlook.com>
2019-07-09 00:42:09 -0400

    Wrap OpenGL texture setting macros

--- MT Game Engine/Texture.cpp
+++ MT Game Engine/Texture.cpp
@@ -1,44 +1,102 @@
 #include "Texture.h"
 
 namespace mtge {
+	//Private
+	void Texture::setTextureWrap_GL(TextureWrapSetting textureWrap, GLint &textureWrap_GL) {
+		switch (textureWrap) {
+		case TextureWrapSetting::REPEAT:
+			textureWrap_GL = GL_REPEAT;
+			break;
+		case TextureWrapSetting::REPEAT_MIRRORED:
+			textureWrap_GL = GL_MIRRORED_REPEAT;
+			break;
+		case TextureWrapSetting::EDGE_CLAMP:
+			textureWrap_GL = GL_CLAMP_TO_EDGE;
+			break;
+		case TextureWrapSetting::BORDER_CLAMP:
+			textureWrap_GL = GL_CLAMP_TO_BORDER;
+		}
+	}
+	void Texture::setTextureFilter_GL(TextureFilterSetting textureFilter, GLint &textureFilter_GL) {
+		switch (textureFilter) {
+		case TextureFilterSetting::NEAREST:
+			textureFilter_GL = GL_NEAREST;
+			break;
+		case TextureFilterSetting::LINEAR:
+			textureFilter_GL = GL_LINEAR;
+		}
+	}
+	void Texture::setColorFormat_GL(TextureColorSetting colorFormat, GLint &colorFormat_GL) {
+		switch (colorFormat) {
+		case TextureColorSetting::RGB:
+			colorFormat_GL = GL_RGB;
+			break;
+		case TextureColorSetting::RGBA:
+			colorFormat_GL = GL_RGBA;
+		}
+	}
+	void Texture::setTextureSettings_GL_2D(TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat) {
+		setTextureWrap_GL(textureWrapX, textureSettings_GL_2D.textureWrapX);
+		setTextureWrap_GL(textureWrapY, textureSettings_GL_2D.textureWrapY);
+
+		setTextureFilter_GL(textureFilterMin, textureSettings_GL_2D.textureFilterMin);
+		setTextureFilter_GL(textureFilterMag, textureSettings_GL_2D.textureFilterMag);
+
+		setColorFormat_GL(colorFormat, textureSettings_GL_2D.colorFormat);
+	}
+	void Texture::setTextureSettings_GL_3D(TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureWrapSetting textureWrapZ, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat) {
+		setTextureWrap_GL(textureWrapX, textureSettings_GL_3D.textureWrapX);
+		setTextureWrap_GL(textureWrapY, textureSettings_GL_3D.textureWrapY);
+		setTextureWrap_GL(textureWrapZ, textureSettings_GL_3D.textureWrapZ);
+
+		setTextureFilter_GL(textureFilterMin, textureSettings_GL_3D.textureFilterMin);
+		setTextureFilter_GL(textureFilterMag, textureSettings_GL_3D.textureFilterMag);
+
+		setColorFormat_GL(colorFormat, textureSettings_GL_3D.colorFormat);
+	}
+
 	//Public
-	void Texture::load2D(const char* textureFile, GLenum target, GLint textureParamX, GLint textureParamY, GLint textureParamMin, GLint textureParamMag, GLenum colorFormat, GLenum totalChannels) {
+	void Texture::load2D(const char* textureFile, TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat) {
+		setTextureSettings_GL_2D(textureWrapX, textureWrapY, textureFilterMin, textureFilterMag, colorFormat);
+
 		//Setup/Bind Texture
 		glGenTextures(1, &textureID);
-		glBindTexture(target, textureID);
+		glBindTexture(GL_TEXTURE_2D, textureID);
 
 		//Set Texture Parameters
-		glTexParameteri(target, GL_TEXTURE_WRAP_S, textureParamX);
-		glTexParameteri(target, GL_TEXTURE_WRAP_T, textureParamY);
-		glTexParameteri(target, GL_TEXTURE_MIN_FILTER, textureParamMin);
-		glTexParameteri(target, GL_TEXTURE_MAG_FILTER, textureParamMag);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, textureSettings_GL_2D.textureWrapX);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, textureSettings_GL_2D.textureWrapY);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, textureSettings_GL_2D.textureFilterMin);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, textureSettings_GL_2D.textureFilterMag);
 
 		//Load Texture
 		stbi_set_flip_vertically_on_load(true);
 		int textureWidth, textureHeight, numColorChannels;
 		unsigned char *data = stbi_load(textureFile, &textureWidth, &textureHeight, &numColorChannels, 0);
 		if (data) {
-			glTexImage2D(target, 0, colorFormat, textureWidth, textureHeight, 0, totalChannels, GL_UNSIGNED_BYTE, data);
-			glGenerateMipmap(target);
+			glTexImage2D(GL_TEXTURE_2D, 0, textureSettings_GL_2D.colorFormat, textureWidth, textureHeight, 0, textureSettings_GL_2D.colorFormat, GL_UNSIGNED_BYTE, data);
+			glGenerateMipmap(GL_TEXTURE_2D);
 		}
 		else {
 			std::cout << "ERROR: TEXTURE FAILED TO LOAD" << std::endl;
 		}
 		stbi_image_free(data);
 
-		this->target = target;
+		this->target = GL_TEXTURE_2D;
 	}
-	void Texture::loadCubemap(const char *textureFiles[], GLenum target, GLint textureParamX, GLint textureParamY, GLint textureParamZ, GLint textureParamMin, GLint textureParamMag, GLenum colorFormat, GLenum totalChannels) {
+	void Texture::loadCubemap(const char *textureFiles[], TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureWrapSetting textureWrapZ, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat) {
+		setTextureSettings_GL_3D(textureWrapX, textureWrapY, textureWrapZ, textureFilterMin, textureFilterMag, colorFormat);
+		
 		//Setup/Bind Texture
 		glGenTextures(1, &textureID);
-		glBindTexture(target, textureID);
+		glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
 		//Set Texture Parameters
-		glTexParameteri(target, GL_TEXTURE_WRAP_S, textureParamX);
-		glTexParameteri(target, GL_TEXTURE_WRAP_T, textureParamY);
-		glTexParameteri(target, GL_TEXTURE_WRAP_R, textureParamZ);
-		glTexParameteri(target, GL_TEXTURE_MIN_FILTER, textureParamMin);
-		glTexParameteri(target, GL_TEXTURE_MAG_FILTER, textureParamMag);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, textureSettings_GL_3D.textureWrapX);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, textureSettings_GL_3D.textureWrapY);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, textureSettings_GL_3D.textureWrapZ);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, textureSettings_GL_3D.textureFilterMin);
+		glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, textureSettings_GL_3D.textureFilterMag);
 
 		//Load Texture
 		stbi_set_flip_vertically_on_load(false);
@@ -46,8 +104,8 @@ namespace mtge {
 		for (unsigned int i = 0; i < 6; i++) {
 			unsigned char *data = stbi_load(textureFiles[i], &textureWidth, &textureHeight, &numColorChannels, 0);
 			if (data) {
-				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, colorFormat, textureWidth, textureHeight, 0, totalChannels, GL_UNSIGNED_BYTE, data);
-				glGenerateMipmap(target);
+				glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, textureSettings_GL_3D.colorFormat, textureWidth, textureHeight, 0, textureSettings_GL_3D.colorFormat, GL_UNSIGNED_BYTE, data);
+				glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
 			}
 			else {
 				std::cout << "ERROR: TEXTURE FAILED TO LOAD" << std::endl;
@@ -55,9 +113,9 @@ namespace mtge {
 			stbi_image_free(data);
 		}
 
-		this->target = target;
+		this->target = GL_TEXTURE_CUBE_MAP;
 	}
-	void Texture::loadSkybox(const std::string FOLDER_NAME, GLenum target, GLint textureParamX, GLint textureParamY, GLint textureParamZ, GLint textureParamMin, GLint textureParamMag, GLenum colorFormat, GLenum totalChannels) {
+	void Texture::loadSkybox(const std::string FOLDER_NAME, TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureWrapSetting textureWrapZ, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat) {
 		std::string skyTexturesString[6] = {
 			"skyFront.tga",
 			"skyBack.tga",
@@ -74,7 +132,7 @@ namespace mtge {
 		for (unsigned int i = 0; i < 6; i++) {
 			skyboxTexturesChar[i] = skyTexturesString[i].c_str();
 		}
-		loadCubemap(skyboxTexturesChar, target, textureParamX, textureParamY, textureParamZ, textureParamMin, textureParamMag, colorFormat, totalChannels);
+		loadCubemap(skyboxTexturesChar, textureWrapX, textureWrapY, textureWrapZ, textureFilterMin, textureFilterMag, colorFormat);
 	}
 	void Texture::setUniform(Shader* shader, unsigned int location, int textureNum) {
 		shader->useProgram();
--- MT Game Engine/Texture.h
+++ MT Game Engine/Texture.h
@@ -9,15 +9,47 @@
 #include "Shader.h"
 
 namespace mtge {
+	enum class TextureWrapSetting {
+		REPEAT, REPEAT_MIRRORED, EDGE_CLAMP, BORDER_CLAMP, 
+	};
+	enum class TextureFilterSetting {
+		NEAREST, LINEAR
+	};
+	enum class TextureColorSetting {
+		RGB, RGBA
+	};
+
 	class Texture {
 	private:
+		struct TextureSettings_GL_2D {
+			GLint textureWrapX;
+			GLint textureWrapY;
+			GLint textureFilterMin;
+			GLint textureFilterMag;
+			GLint colorFormat;
+		} textureSettings_GL_2D;
+		struct TextureSettings_GL_3D {
+			GLint textureWrapX;
+			GLint textureWrapY;
+			GLint textureWrapZ;
+			GLint textureFilterMin;
+			GLint textureFilterMag;
+			GLint colorFormat;
+		} textureSettings_GL_3D;
+
 		unsigned int textureID;
 		GLenum target;
 
+		void setTextureWrap_GL(TextureWrapSetting textureWrap, GLint &textureWrap_GL);
+		void setTextureFilter_GL(TextureFilterSetting textureFilter, GLint &textureFilter_GL);
+		void setColorFormat_GL(TextureColorSetting colorFormat, GLint &colorFormat_GL);
+		void setTextureSettings_GL_2D(TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat);
+		void setTextureSettings_GL_3D(TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureWrapSetting textureWrapZ, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat);
+
 	public:
-		void load2D(const char* textureFile, GLenum target, GLint textureParamX, GLint textureParamY, GLint textureParamMin, GLint textureParamMag, GLenum colorFormat, GLenum totalChannels);
-		void loadCubemap(const char *textureFiles[], GLenum target, GLint textureParamX, GLint textureParamY, GLint textureParamZ, GLint textureParamMin, GLint textureParamMag, GLenum colorFormat, GLenum totalChannels);
-		void loadSkybox(const std::string FOLDER_NAME, GLenum target, GLint textureParamX, GLint textureParamY, GLint textureParamZ, GLint textureParamMin, GLint textureParamMag, GLenum colorFormat, GLenum totalChannels);
+		void load2D(const char* textureFile, TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat);
+		void loadCubemap(const char *textureFiles[], TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureWrapSetting textureWrapZ, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat);
+		void loadSkybox(const std::string FOLDER_NAME, TextureWrapSetting textureWrapX, TextureWrapSetting textureWrapY, TextureWrapSetting textureWrapZ, TextureFilterSetting textureFilterMin, TextureFilterSetting textureFilterMag, TextureColorSetting colorFormat);
 		void setUniform(Shader* shader, unsigned int location, int textureNum);
 		void activate(GLenum textureNum);
 	};
